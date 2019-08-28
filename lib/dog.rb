3bdc441033a9ee7a6fa25b4e57e91b6cc138d30b
JazzyMussels <jeffmussels@gmail.com>
2019-08-28 11:55:32 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+
+  @@dogs = []
+
+  attr_reader :name 
+  attr_accessor :owner, :mood 
+   def initialize(name, owner)
+    @name = name
+    @owner = owner 
+    @mood = 'nervous'
+    @@dogs << self
+  end
+
+  def self.all
+    @@dogs 
+  end
+
 end
