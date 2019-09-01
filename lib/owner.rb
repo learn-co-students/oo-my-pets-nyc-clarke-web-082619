836b806867b5cbfe85e6b6ed8d216c8a5f80ca27
github username <github email address>
2019-09-01 13:16:59 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
-end
+  attr_reader :name, :species
+  attr_accessor  
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self 
+  end 
+
+  def say_species
+    "I am a #{self.species}."
+  end 
+
+  def cats
+    Cat.all.select {|cats| cats.owner == self }
+  end 
+  
+  def dogs
+    Dog.all.select {|dogs| dogs.owner == self }
+  end 
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    dogs.map {|dog| dog.mood = "happy"}
+  end 
+
+  def feed_cats
+    cats.map {|cat| cat.mood = "happy"}
+  end 
+
+  def sell_pets
+    pets = [] 
+    pets << dogs
+    pets << cats 
+    # Cat.all.collect {|cats| pets << cats if cats.owner == self}
+    # Dog.all.collect {|dogs| pets << dogs if dogs.owner == self }
+    pets.flatten.map {|pets| pets.mood = "nervous" }
+    pets.flatten.map { |pets| pets.owner = nil }
+  end 
+
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end 
+  
+  def self.all
+    @@all
+  end 
+
+  def self.count
+    self.all.count 
+  end 
+
+  def self.reset_all
+    self.all.clear 
+  end 
+
+end 
